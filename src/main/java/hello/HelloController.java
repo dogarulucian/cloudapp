d2bbac7623b10e1e41494ad03a2657403c8446d2
Lucian Dogaru <Lucian.Dogaru@endava.com>
2018-06-27 11:47:51 +0300

    Test

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("redirect:https://pbs.twimg.com/media/DAcNeFqXgAAx1mh.jpg:large");
+    }
+    
+}
